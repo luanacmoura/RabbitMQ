1040a1be2f680e0783000c619eefda0d3979bfae
Luana <luana.moura@intelitrader.com.br>
2021-01-14 13:28:25 -0300

    Add functional consumer and producer

---
+++ Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+
+                        channel.BasicAck(ea.DeliveryTag, false); // se for true, então vai confirmar o recebimento de todas as mensagems com aquela deliverytag
+
+                        //channel.BasicReject(ea.DeliveryTag, true);
+
+                        //channel.BasicNack(ea.DeliveryTag, true, true);
+                        Console.WriteLine(" [x] Mensagem recebida: {0}", message);
+                    };
+
+                    channel.BasicConsume(queue: "Fila1",
+                                         autoAck: false,
+                                         consumer: consumer);
+                    
+                    Console.WriteLine(" Pressione qualquer tecla para sair.");
+                    Console.ReadLine();
+
+                }
+            }
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,60 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Insira a mensagem que deseja enviar:");
+                var message = Console.ReadLine();
+                Send(message);
+            }
+        }
+
+        public static void Send(string message)
+        {
+            //Criando a conexão com o servidor
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                //Criando o nosso canal
+                using (var channel = connection.CreateModel())
+                {
+                    //channel.ExchangeDeclarePassive;
+                    channel.ExchangeDeclare(exchange: "Exchange1", type: ExchangeType.Fanout);
+
+                    //Declarando a fila
+                    //channel.QueueDeclarePassive
+                    channel.QueueDeclare(queue: "Fila1",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        null);
+
+                    channel.QueueBind(queue: "Fila1",
+                              exchange: "Exchange1",
+                              routingKey: "Chave1"); //Também chamada de binding key
+                    //se o exchange for do tipo fanout ele vai ignorar a routing key
+
+
+                    var body = Encoding.UTF8.GetBytes(message); 
+
+                    //Enviando uma mensagem pra fila que criamos (subscribe)
+                    channel.BasicPublish(exchange: "Exchange1", //se estiver vazio, por padrão utiliza uma direct exchange
+                        routingKey: "Chave1", // se o exchange estiver vazio, precisa ser igual ao nome da fila para que ele a encontre
+                        basicProperties: null,
+                        body: body);
+
+                    Console.WriteLine(" [x] Mensagem enviada: {0}", message);
+                    Console.ReadLine();
+
+                }
+            }
+        }
+    }
+}
